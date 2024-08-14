998ded8ee6d39777a3448cbade7f7578279dc036
Janak Chaudhary <janak.chaudhary@bajratechnologies.com>
2024-08-14 17:40:51 +0545

    feat:add head,primary card and mainlayout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { PrimaryCardComponent } from './shared/components/primary-card/primary-card.component'
+import { MatCardModule } from '@angular/material/card'
+import { MatDividerModule } from '@angular/material/divider';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HeadCardComponent } from './shared/components/head-card/head-card.component'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PrimaryCardComponent, MainLayoutComponent, HeadCardComponent],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule, MatDividerModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
