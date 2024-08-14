c4373f229e2f08b233172a4cb81d6b0bb44edb45
Rajan Chaudhary <rajan.chaudhary@bajratechnologies.com>
2024-08-14 17:37:46 +0545

    feat: add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { NavbarComponent } from './shared/components/Navbar/navbar/navbar.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule
   ],
   providers: [
     provideAnimationsAsync()
